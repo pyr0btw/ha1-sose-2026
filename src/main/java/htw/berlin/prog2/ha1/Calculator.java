8c08d8d4a7df2ba1656779a533b988a872bb8063
pyro <pyroholl@gmail.com>
2026-04-15 14:04:04 +0200

    Commit: Korrektur in pressDigitKey

--- src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -32,7 +32,7 @@ public class Calculator {
         if(digit > 9 || digit < 0) throw new IllegalArgumentException();
 
         if(screen.equals("0") || screen.equals("-0") || latestValue == Double.parseDouble(screen)) {
-          if (screen.startsWith("-")){
+          if (screen.equals("-0")){
               screen = "-";
           } else {
               screen = "";
